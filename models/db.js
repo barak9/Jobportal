eb9a7170d436ee1284de0f2a3a443d5bd9fa2cd9
barak9 <31530635+barak9@users.noreply.github.com>
2023-04-15 11:30:14 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -5,8 +5,7 @@ mongoose.connect(DB,
 
 { useNewUrlParser: true,  
     useUnifiedTopology: true,
-useNewUrlParser:true,
-useFindAndModify:false},
+useNewUrlParser:true},
 
 (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
